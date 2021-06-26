7556a910dc9b10ea62ff6ebea33d7f5c75d7c58f
Franc Cosp <franccospmeyaeso@gmail.com>
2021-06-26 09:58:24 +0000

    creating models

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # student = db.Column(db.Boolean(), unique=False, nullable=False)
+    # teacher = db.Column(db.Boolean(), unique=False, nullable=False)
+    promo = db.Column(db.Boolean(), unique=False, nullable=False)
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +24,4 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
