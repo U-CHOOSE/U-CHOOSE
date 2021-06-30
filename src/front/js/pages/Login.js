5846a49c4278fc59d53b63e9a607bf644674d090
tapuks <david_berdiell@hotmail.com>
2021-06-30 20:20:39 +0000

    add html

--- src/front/js/pages/Login.js
+++ src/front/js/pages/Login.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-	return <div>Login</div>;
+	const [email, SetEmail] = useState("");
+	const [password, SetPassword] = useState("");
+	function login() {
+		fetch("", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				email: email,
+				password: password
+			})
+		})
+			.then(response => response.json())
+			.then(responseJson => {
+				console.log(responseJson);
+				// history.push("/LandingPage");
+				// o buscar profesor
+			});
+	}
+
+	return (
+		<div>
+			<h1 className="title">Acceder</h1>
+			<form>
+				<div className="form-group">
+					<input
+						type="email"
+						className="form-control"
+						aria-describedby="emailHelp"
+						placeholder="Enter email"
+						onChange={event => SetEmail(event.target.value)}
+					/>
+				</div>
+				<div className="form-input">
+					<input
+						type="password"
+						className="form-control"
+						id="exampleInputPassword1"
+						placeholder="Password"
+						onChange={event => SetEmail(event.target.value)}
+					/>
+				</div>
+				<a className="link" href="#">
+					¿Olvidaaste tu contraseña?
+				</a>
+				<br />
+
+				<button
+					type="submit"
+					className="btn btn-primary"
+					onClick={event => {
+						event.preventDefault();
+						login();
+					}}>
+					Iniciar sesión
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
