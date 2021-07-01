0e97ddb40b9c7530d2515272ffd25fe3f8dca6a4
Kty <katiuska.rodriguez5@gmail.com>
2021-07-01 13:20:47 +0000

    footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -6,16 +6,12 @@ const Footer = () => {
 	const history = useHistory();
 	return (
 		<footer className="footer mt-auto py-3 text-center">
-			<div onClick={() => history.push("/")}>
-				{" "}
-				<button className="btn btn-primary" href="#" role="button">
-					Buscar Centro
-				</button>
-				<br />
-				<button className="btn btn-primary" href="#" role="button">
-					Buscar Profesor
-				</button>
-			</div>
+			<div onClick={() => history.push("/")}> Listado de Centros</div>
+			<div onClick={() => history.push("/")}> Listado de Profesores</div>
+			<div onClick={() => history.push("/")}> Politicas de Cookies</div>
+			<div onClick={() => history.push("/")}> Terminos de Uso</div>
+			<div onClick={() => history.push("/")}> Politicas de Privacidad</div>
+			<div onClick={() => history.push("/")}> Contactos</div>
 		</footer>
 	);
 };
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
 	const history = useHistory();
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<div onClick={() => history.push("/")}> LOGO</div>
-			<div onClick={() => history.push("/")}> </div>
+			<div onClick={() => history.push("/")}>LOGO </div>
+			<image src="/dist/asseLogo-Icon" roundedCircle /> <div onClick={() => history.push("/")}> MENU </div>
 		</nav>
 	);
 };
