ac10537ad389f56b3be124b171ac4c6e2c81ceab
tapuks <david_berdiell@hotmail.com>
2021-07-02 12:30:13 +0000

    responsive test

--- src/front/js/pages/LandingPage.js
+++ src/front/js/pages/LandingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import reviewsImg from "../../../../docs/assets/mockups_reviews.jpg";
 import reviewMetrics from "../../../../docs/assets/review_metrics.png";
 import logo from "../../../../docs/assets/Logo_U-CHOOSE.jpg";
@@ -8,37 +8,67 @@ import "../../styles/landingPage.scss";
 // import favicon from "./favicon.png";
 
 const LandingPage = () => {
+	const [width, setWidth] = useState(null);
+	useEffect(() => {
+		const resizeW = () => setWidth(window.innerWidth);
+
+		window.addEventListener("resize", resizeW); // Update the width on resize
+		console.log(width);
+		return () => window.removeEventListener("resize", resizeW);
+	});
 	return (
 		<div>
 			{/* <navbar/> */}
-			<img className="img1" src={logo} />
+			{width < 1000 ? <img className="img1" src={logo} /> : ""}
 			{/* <img src={favicon} />  */}
+			<div className="row">
+				<div className="col-sm-12 col-lg-6">
+					<h1 className="black_h1 title">Te ayudamos a elegir dónde estudiar </h1>
+					<p>
+						Acertar con tus estudios es difícil.
+						<br />
+						U-Choose te lo hace más fácil gracias a las reviews sobre los profesores.
+					</p>
+					<div className="contain-buttons d-flex flex-column">
+						<button type="button" className="button_violet_great btn-school">
+							Buscar un centro
+						</button>
+						<button type="button" className="button_white_border_violet_great btn-teacher ">
+							Buscar un profesor
+						</button>
+					</div>
+				</div>
+				<div className="col-sm-12 col-lg-6">
+					{width < 1000 ? (
+						<>
+							{" "}
+							<h2 className="black_h2 subtitle2 ">¿Cómo funciona?</h2>{" "}
+							<h3 className="black_h2 subtitle3">Recogemos datos reales</h3>
+						</>
+					) : (
+						""
+					)}
 
-			<h1 className="black_h1 title">Te ayudamos a elegir dónde estudiar </h1>
-			<p>
-				Acertar con tus estudios es difícil.
-				<br />
-				U-Choose te lo hace más fácil gracias a las reviews sobre los profesores.
-			</p>
-
-			<div className="contain-buttons d-flex flex-column">
-				<button type="button" className="button_violet_great btn-school">
-					Buscar un centro
-				</button>
-				<button type="button" className="button_white_border_violet_great btn-teacher ">
-					Buscar un profesor
-				</button>
+					<img className="img2" src={reviewsImg} />
+				</div>
+				<div className="col-sm-12 col-lg-6">
+					{width > 1000 ? (
+						<>
+							{" "}
+							<h2 className="black_h2 subtitle2 ">¿Cómo funciona?</h2>{" "}
+							<h3 className="black_h2 subtitle3">Recogemos datos reales</h3>
+						</>
+					) : (
+						""
+					)}
+					<p className="paragraph">
+						Alumnos y ex alumnos envían <span className="font-weight-bold">reviews verificadas</span> de
+						profesores y en base a los criterios que determinan tu experiencia: el{" "}
+						<span className="font-weight-bold">pasión, ejemplos prácticos, dinamismo</span> e{" "}
+						<span className="font-weight-bold">implicaicón.</span>
+					</p>
+				</div>
 			</div>
-
-			<h2 className="black_h2 subtitle2 ">¿Cómo funciona?</h2>
-			<h3 className="black_h2 subtitle3">Recogemos datos reales</h3>
-			<img className="img2" src={reviewsImg} />
-			<p className="paragraph">
-				Alumnos y ex alumnos envían <span className="font-weight-bold">reviews verificadas</span> de profesores
-				y en base a los criterios que determinan tu experiencia: el{" "}
-				<span className="font-weight-bold">pasión, ejemplos prácticos, dinamismo</span> e{" "}
-				<span className="font-weight-bold">implicaicón.</span>
-			</p>
 			<button type="button" className="button_marino_great btn-review">
 				Hacer una review
 			</button>
