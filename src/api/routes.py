ae846213ab6f2bd2121767d854e7a66f35e3fb33
Franc Cosp <franccospmeyaeso@gmail.com>
2021-07-05 15:37:48 +0000

    all right

--- src/api/routes.py
+++ src/api/routes.py
@@ -29,48 +29,49 @@ def get_all_users():
 
 @api.route('/users', methods = ['POST'])
 def create_user():
-    try:
-        full_name = request.json.get('full_name', None)
-        email = request.json.get('email', None)
-        password = request.json.get('password', None)
-        is_active = request.json.get('is_active', None)
-        is_student = request.json.get('student', None)
-        promo = request.json.get('promo', None)
+    full_name = request.json.get('full_name', None)
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    is_active = request.json.get('is_active', None)
+    is_student = request.json.get('student', None)
+    promo = request.json.get('promo', None)
 
-        if not (email and promo and is_active and password and student or teacher):
-            return {'error': 'Missing info'}, 400
+    if not (email and promo and is_active and password and student or teacher):
+        return {'error': 'Missing info'}, 400
 
-        user = User.get_by_email(email)
-        if user : 
-            return jsonify ({"error" : " email  already used"})
-        
-        new_user = User(
-            email=email,
-            password = password,
-            is_active = is_active,
-            promo= promo,
-            is_student = is_student
-        )
-        create_user = new_user.create()
-        
-        # if user and not user.is_active:
-        #     user.reactive_account(email,student or teacher, password)
-        #     return jsonify(user), 200
-
-        if is_student == True : 
-            new_student = Student(
-                full_name = full_name,
-                user_id = create_user.id
-            )
-            create_student = new_student.create()
-
-            return jsonify({"user": create_user,"student" : create_student})
-        else : 
-            new_teacher = Teacher(
-                full_name = full_name,
-                user_id = create_user.id
-            )
+    user = User.get_by_email(email)
+    if user : 
+        return jsonify ({"error" : " email  already used"})
     
+    new_user = User(
+        email=email,
+        password = password,
+        is_active = is_active,
+        promo= promo,
+        is_student = is_student
+    )
+    create_user = new_user.create()
+    
+    # if user and not user.is_active:
+    #     user.reactive_account(email,student or teacher, password)
+    #     return jsonify(user), 200
+
+    if is_student == True : 
+        new_student = Student(
+            full_name = full_name,
+            user_id = create_user.id
+        )
+        create_student = new_student.create()
+
+        return jsonify({"user": create_user,"student" : create_student})
+    else : 
+        new_teacher = Teacher(
+            full_name = full_name,
+            user_id = create_user.id
+        )
+    create_teacher = new_teacher.create()
+
+    return jsonify({"user": create_user,"teacher" : create_teacher})
 
 
 
