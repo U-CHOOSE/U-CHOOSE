cca994ae291e6e6bc6e6bac5225c67e99e1f3cc3
Franc Cosp <franccospmeyaeso@gmail.com>
2021-07-05 16:01:44 +0000

    open other gitpod

--- src/api/models.py
+++ src/api/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     is_student = db.Column(db.Boolean(), unique=False, nullable=False)
     promo = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -24,6 +24,7 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "password" : self.password,
             "is_active": self.is_active,
             "student": self.student,
             "teacher": self.teacher,
