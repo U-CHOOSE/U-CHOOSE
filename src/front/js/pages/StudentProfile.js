13ee9622f2b5a36ef25ea9e56d133e67eade96d8
tapuks <david_berdiell@hotmail.com>
2021-07-07 17:15:14 +0000

    ok

--- src/front/js/pages/StudentProfile.js
+++ src/front/js/pages/StudentProfile.js
@@ -4,6 +4,9 @@ import Modal from "../component/Modal/Modal";
 
 const StudentProfile = () => {
 	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [teacher, setTeacher] = useState("");
+	const [password, setPassword] = useState("");
 	return (
 		<div>
 			<div className="actions">{/* flecha
@@ -22,16 +25,16 @@ const StudentProfile = () => {
 					onChange={event => setName(event.target.value)}
 				/>
 				<input
-					type="text"
-					className="form-control input-text"
-					placeholder="Nombre completo"
+					type="email"
+					className="form-control input-email"
+					placeholder="Email"
 					onChange={event => setEmail(event.target.value)}
 				/>
 				<input
 					type="text"
 					className="form-control input-text"
-					placeholder="Nombre completo"
-					onChange={event => setName(event.target.value)}
+					placeholder="¿De qué eres profesor?"
+					onChange={event => setTeacher(event.target.value)}
 				/>
 				<button className="button1">Mis centros</button>
 			</div>
