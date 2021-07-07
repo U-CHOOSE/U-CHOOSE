ecd91f2fa4d33eae0a739d821b15e93941926cdd
tapuks <david_berdiell@hotmail.com>
2021-07-07 16:23:04 +0000

    create branch

---
+++ src/front/js/pages/StudentProfile.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useContext } from "react";
+import "../../styles/studentProfile.scss";
+
+const StudentProfile =()=> {
+    return (
+        <div>StudentProfile</div>
+    )
+}
+
+export default StudentProfile;
