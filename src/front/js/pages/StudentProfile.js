65d93254eb3227dcd2d6df6b517fd044a78185d6
tapuks <david_berdiell@hotmail.com>
2021-07-09 09:35:35 +0000

    html and css

--- src/front/js/pages/StudentProfile.js
+++ src/front/js/pages/StudentProfile.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
 import "../../styles/studentProfile.scss";
-import Modal from "../component/Modal/Modal";
 
 const StudentProfile = () => {
 	const [name, setName] = useState("");
@@ -14,68 +13,63 @@ const StudentProfile = () => {
 				salir */}</div>
 
 			<div className="contain1">
-				<img className="img-profile" url="" />
+				<img
+					className="img-profile"
+					src="https://laverdadnoticias.com/__export/1577809178240/sites/laverdad/img/2019/12/31/1465326837-rt-shakira01.jpg_1017309733.jpg"
+					alt="img"
+				/>
 				<button className="button1">Mis centros</button>
 			</div>
 
 			<div className="contain-inputs">
-				<div>
-					<label className="input">Nombre completo</label>
+				<div className="contain-inp">
+					<label>Nombre completo</label>
 					<input
 						type="text"
-						className="form-control input-text input"
-						placeholder=""
+						className="form-control input"
+						placeholder="eefew"
 						onChange={event => setName(event.target.value)}
 					/>
 				</div>
-				{/* <div>
-					<label>Email address</label>
-					<input
-						type="text"
-						className="form-control input-text inp"
-						placeholder="Nombre completo"
-						onChange={event => setName(event.target.value)}
-					/>
-				</div>
-				<div>
-					<label>Email address</label>
+				<div className="contain-inp">
+					<label>Email</label>
 					<input
 						type="email"
-						className="form-control input-email inp"
-						placeholder="Email"
+						className="form-control input-text inp"
+						placeholder="Nombre completo"
 						onChange={event => setEmail(event.target.value)}
 					/>
 				</div>
-				<div>
-					<label>Email address</label>
+				<div className="contain-inp">
+					<label>¿De qué eres profesor?</label>
 					<input
 						type="text"
-						className="form-control input-text inp"
-						placeholder="¿De qué eres profesor?"
+						className="form-control input-email inp"
+						placeholder="ffdf"
 						onChange={event => setTeacher(event.target.value)}
 					/>
 				</div>
-				<div>
-					<label>Email address</label>
+				<div className="contain-inp">
+					<label>Contraseña</label>
 					<input
 						type="password"
-						className="form-control input-password inp"
-						placeholder="contraseña"
+						className="form-control input-text inp"
+						placeholder=""
 						onChange={event => setPassword(event.target.value)}
 					/>
 				</div>
-				<div>
-					<label>Email address</label>
+				<div className="contain-inp">
+					<label>Repetir contraseña</label>
 					<input
 						type="password"
-						className="inp form-control input-password inp"
-						placeholder="Repetir contraseña"
+						className="form-control input-password inp"
+						placeholder=""
 						onChange={event => setPassrepeat(event.target.value)}
 					/>
-				</div> */}
+				</div>
 			</div>
 
-			{/* <button className="button1">Guardar</button> */}
+			<button className="button2">Guardar</button>
 		</div>
 	);
 };
