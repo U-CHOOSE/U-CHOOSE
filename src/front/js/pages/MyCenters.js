9d869024f64726548e770abd509fcace00748d2a
tapuks <david_berdiell@hotmail.com>
2021-07-12 09:52:28 +0000

    search ok

--- src/front/js/pages/MyCenters.js
+++ src/front/js/pages/MyCenters.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect, useContext } from "react";
 import "../../styles/myCenters.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import Search from "../component/Search/Search";
 
 const MyCenters = () => {
 	return (
-		<div>
-			<h1 className="title">Mis centros</h1>
-			<Search title="fdsfd" placeholder="fefsfsdf" />
+		<div className="contain">
+			<Search
+				title="Mis centros"
+				placeholder="Busca un centro"
+				span1="¿No encuentras tu centro?"
+				button="Guardar"
+			/>
 		</div>
 	);
 };
