4ef273429cba1de853924ba7c1cbbf8987124682
Franc Cosp <franccospmeyaeso@gmail.com>
2021-07-15 18:31:17 +0000

    doing all CRUD without schhols

--- src/api/models.py
+++ src/api/models.py
@@ -18,6 +18,7 @@ class User(db.Model):
     is_student = db.Column(db.Boolean)
     user_student = db.relationship('User_student', cascade="all, delete", lazy=True)
     user_teacher = db.relationship("User_teacher", cascade="all, delete", lazy=True)
+    user_school = db.relationship("User_school", cascade="all, delete", lazy=True)
 
 
     def __repr__(self):
@@ -51,9 +52,11 @@ class User(db.Model):
     def get_by_id(cls, id):
         user = cls.query.filter_by(id = id).first()
         return user
+        
     @classmethod
     def get_all(cls):
-        cls.query.all()
+        users = cls.query.all()
+        return users
 
     @classmethod
     def update_single_user(cls, user_data, id):
@@ -168,7 +171,7 @@ class School(db.Model):
 
 
     def __repr__(self):
-        return '<Teacher %r>' % self.full_name
+        return '<School %r>' % self.name
 
     def serialize(self):
         return {
@@ -176,6 +179,19 @@ class School(db.Model):
             "name": self.name
         }
 
+    @classmethod
+    def get_all(cls):
+        schools = cls.query.all()
+        return schools
+
+    def set_with_json(self,json):
+        self.name = json["name"]
+        self.img = json["img"]
+    
+    def db_post(self):        
+        db.session.add(self)
+        db.session.commit()
+        
 class User_school(db.Model):
     __tablename__ = 'user_school'
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),primary_key=True)
@@ -199,150 +215,7 @@ class User_school(db.Model):
 
         }
 
-
-# class User_teacher_school(db.Model):
-#     __tablename__ = 'user_teacher_school'
-#     id = db.Column(db.Integer, primary_key=True)
-#     user_teacher_id = db.Column(db.Integer, db.ForeignKey('User_teacher.id'))
-#     user_teacher = db.relationship("User_student", back_populates="school_teacher")
-
-
-
-# class Teacher(db.Model,BaseModel):
-#     __tablename__ = 'teacher'
-#     id = db.Column(db.Integer, primary_key=True)
-#     full_name = db.Column(db.String(120), unique=False, nullable=False)
-#     email =db.Column(db.String(120), nullable=False)
-#     password =db.Column(db.String(50), nullable=False)
-#     linkedin = db.Column(db.String(120), unique=False, nullable=True)
-#     type_of_teacher = db.Column(db.String(120), unique=False, nullable=False)
-#     is_logged=db.Column(db.Boolean, default=False, nullable=False)
-#     promo=db.Column(db.Boolean, default=False, nullable=False)
-#     schools = db.relationship('School', backref='teacher', lazy=True)
-
-
-#     def __repr__(self):
-#         return '<Teacher %r>' % self.full_name
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "full_name": self.full_name,
-#             "email": self.email,
-#             "linkedin": self.linkedin,
-#             "type_of_teacher": self.type_of_teacher,
-#             "promo": self.promo,
-#             "schools": list(map(lambda x: x.serialize(), self.schools))
-#         }
-    
-#     def create(self):
-#         db.session.add(self)
-#         db.session.commit()
-    
-#     def db_delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
-
-
-# class School(db.Model,BaseModel):
-#     __tablename__ = 'school'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(120), unique=False, nullable=False)
-#     img = db.Column(db.String(250), unique=False, nullable=False)
-#     students = db.Column(db.Integer, db.ForeignKey('student.id'))
-#     teachers = db.Column(db.Integer, db.ForeignKey('teacher.id'))
-
-
-#     def __repr__(self):
-#         return '<Teacher %r>' % self.full_name
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "name": self.name,
-#             "students": list(map(lambda x: x.serialize(), self.students)),
-#             "teachers": list(map(lambda x: x.serialize(), self.teachers))
-#         }
-
-# class School_Student(db.Model,BaseModel):
-#     __tablename__ = 'school'
-#     id = db.Column(db.Integer, primary_key=True)
-#     student_id = db.Column(db.String(120), unique=False, nullable=False)
-#     img= db.Column(db.String(250), unique=False, nullable=False)
-
-#     def __repr__(self):
-#         return '<Favorite_Planets %r>' % self.id
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "user_id": self.user_id,
-#             "planet_id":self.planet_id,
-#             }
-
-# class School_Teacher(db.Model,BaseModel):
-#     __tablename__ = 'school'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(120), unique=False, nullable=False)
-#     img= db.Column(db.String(250), unique=False, nullable=False)
-    
-
-
-#     def __repr__(self):
-#         return '<School %r>' % self.name
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "name": self.name,
-#             "img_url": self.img_url
-#         }
-# class School_Student(db.Model):
-#     __tablename__ = 'school_user'
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),primary_key=True)
-#     school_id = db.Column(db.Integer, db.ForeignKey('school.id'),primary_key=True)
-#     user = db.relationship("User")
-#     school = db.relationship("School")
-    
-#     def __repr__(self):
-#         return '<School_User %r>' % self.id
-
-#     def serialize(self):
-#         return {
-#             "id": self.id,
-#             "user_id": self.user_id,
-#             "school_id": self.school_id
-#         }
-
-# class Review_teacher(db.Model):
-#     __tablename__ = 'review_teacher'
-#     id = db.Column(db.Integer, primary_key=True)
-#     dynamsim = db.Column(db.Integer())
-#     pasion = db.Column(db.Integer())
-#     practises_example = db.Column(db.Integer())
-#     near = db.Column(db.Integer())
-#     date_teacher = db.Column(db.Date(), unique=False, nullable=False)
-#     more_info = db.Column(db.String(500), unique=False, nullable=True)
-#     gif = db.Column(db.String(50), unique=False, nullable=True)
-
-    # FALTABA MORE_INFO Y GIF(STRING????)
-    # DYNAMSIM, PASION, NEAR... COMO SON VOTADOS PONEMOS TIPO Integer???
-
-
-
-    # def __repr__(self):
-    #     return '<User %r>' % self.id
-
-    # def serialize(self):
-    #     return {
-    #         "id": self.id,
-    #         "dynamsim": self.dynamsim,
-    #         "pasion": self.pasion,
-    #         "practises_example": self.practises_example,
-    #         "near": self.near,
-    #         "date_teacher": self.date_teacher,
-    #         "more_info": self.more_info,
-    #         "gif": self.gif,
-    #         # do not serialize the password, its a security breach
-    #     }
-
+    @classmethod
+    def get_by_user_id(cls, user):
+        user_company = cls.query.filter_by(user_id=user).first()
+        return user_company
--- src/api/routes.py
+++ src/api/routes.py
@@ -116,121 +116,24 @@ def update_users(id):
         print(user_comp)
         return jsonify(user_comp.to_dict())
 
+# schools
+@api.route('/schools',methods=['GET'])
+def get_all_schools():
+    schools = School.get_all()
+    print(schools)
+    school_dic = []
+    for school in schools:
+        school_dic.append(school.serialize())
+    return jsonify(school_dic), 200
 
+@api.route('/school', methods=['POST'])
+def add_school():
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # @api.route('/student', methods = ['POST'])
-# # def create_student():
-# #     full_name = request.json.get('full_name', None)
-# #     email = request.json.get('email', None)
-# #     password = request.json.get('password', None)
-# #     is_active = request.json.get('is_active', None)
-# #     promo = request.json.get('promo', None)
-# #     schools = request.json.get('schools', None)
-
-# #     if (full_name is None or email is None or password is None or promo is None):
-# #         return {"error":"Missing info "}, 400 
-
-# #     # if user : 
-# #     #     return jsonify ({"error" : " email  already used"}),400
-
-# #         new_student = Student(
-# #             full_name =full_name,
-# #             email= email,
-# #             password= password,
-# #             is_active= is_active,
-# #             promo = promo,
-# #             schools = schools
-# #         )
-
-# #         create_student = new_student.create()
-
-# #         return jsonify(create_student),201
-
-
-
-# @api.route('/student', methods=['POST'])
-# def create_student():
-#     json = request.get_json()
-
-#     if json is None :
-#         return ("error missing info "),400
-
-#     student = Student()
-#     student.set_with_json(json)
-#     student.create()
-
-#     return jsonify(student.serialize()),201
-
-# @api.route("/student/<int:id>",methods=['PUT'])
-# def update_one_student (id):
-#     json = request.get_json()
-#     student = Student.get_one_by_id(id)
-
-#     # if student ["is_logged"] = False : 
-#     #     return ("You can't change your info if you doesn't have account") , 401
-    
-#     student.put_with_json(json)
-#     db.session.commit()
-#     return jsonify(student.serialize()), 201
-
-# @api.route('/student', methods=['GET'])
-# def get_all_students():
-#     students = Student.get_all()
-#     student_dic = []
-#     for student in students:
-#         student_dic.append(student.serialize())
-#     return jsonify(student_dic),200
-
-# @api.route("/student/<int:id>", methods=['GET'])
-# def get_one_student(id):
-#     student = Student.get_one_by_id(id)
-#     return jsonify(student.serialize()),200 
-
-# @api.route("/student/<int:id>", methods=['DELETE'])
-# def delte_one_student(id):
-#     json = request.get_json()
-#     student = Student.get_one_by_id(id)
-#     db.session.delete(student)
-#     db.session.commit()
-#     return jsonify(student.serialize(), "msg: Student  deleted"), 201
-
-
-
+    body = request.get_json()
+    school = School()
+    school.set_with_json(body)
+    school.db_post()
+    return jsonify(school.serialize()),201
 
 
 
