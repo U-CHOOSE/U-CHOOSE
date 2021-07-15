1d5a330e8126bfafe8c91c6fe8f4d4f59411cb3e
Kty <katiuska.rodriguez5@gmail.com>
2021-07-15 17:39:24 +0000

    copy of NavBar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,6 +7,7 @@ import logo from "../../../../docs/assets/img/Logo-Icon.jpg";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import barSolid from "../../../../docs/assets/img/bars-solid.svg";
 import Modal from "./Modal/Modal";
+import { Link } from "react";
 
 const Navbar = () => {
 	const history = useHistory();
@@ -14,15 +15,18 @@ const Navbar = () => {
 	// const handleShowmobilMenu = () => {
 	// 	setshowMobileMenu(!showMobileMenu);
 	const [show, setShow] = useState(false);
-	return (
-		<nav className="navbar navbar-light header d-flex justify-content-between bg-light">
-			<a className="navbar-brand" href="/">
-				<img src={logo} alt="" />
-			</a>
 
-			<button onClick={() => setShow(!show)}> modal</button>
-			{show ? <Modal body={<barSolid />} /> : ""}
-		</nav>
+	return (
+		<>
+			<nav className="navbar navbar-light header d-flex justify-content-between bg-light">
+				<img src={logo} alt="" />
+				<button onClick={() => setShow(!show)}>
+					{" "}
+					<span className="navbar-toggler-icon" />{" "}
+				</button>
+				{show ? <Modal body={screen} /> : ""}{" "}
+			</nav>
+		</>
 	);
 };
 
