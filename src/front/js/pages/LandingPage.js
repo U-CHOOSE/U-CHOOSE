e25021d4174316042b4a9925ecdac97de8399e68
tapuks <david_berdiell@hotmail.com>
2021-07-16 16:39:05 +0000

    responsive ok

--- src/front/js/pages/LandingPage.js
+++ src/front/js/pages/LandingPage.js
@@ -26,18 +26,18 @@ const LandingPage = () => {
 				</div>
 			</div>
 			<div className="row">
-				<div className="col-sm-12 col-lg-6 mt-4 ">
+				<div className="col-sm-12 col-lg-6 mt-4 col1-desktop ">
 					<h1 className="black_h1 title">Te ayudamos a elegir dónde estudiar </h1>
 					<p className="mt-3 paragraph1">
 						Acertar con tus estudios es difícil.
 						<br />
 						U-Choose te lo hace más fácil gracias a las reviews sobre los profesores.
 					</p>
-					<div className="contain-buttons d-flex">
+					<div className="contain-buttons">
 						<button type="button" className="button_violet_great btn-school mt-3">
 							Buscar un centro
 						</button>
-						<button type="button" className="button_white_border_violet_great btn-teacher mt-2 ">
+						<button type="button" className="button_white_border_violet_great btn-teacher mt-3 ">
 							Buscar un profesor
 						</button>
 					</div>
@@ -48,7 +48,7 @@ const LandingPage = () => {
 			</div>
 
 			<div className="row">
-				<div className="col-sm-12 col-lg-4">
+				<div className="col-sm-12 col-lg-4 col3desktop">
 					<h2 className="black_h2 subtitle2 mt-5">¿Cómo funciona?</h2>{" "}
 					<h3 className="black_h2 subtitle3 mt-4">Recogemos datos reales</h3>
 					<img className="img2 mt-2" src={reviewsImg} />
@@ -60,13 +60,18 @@ const LandingPage = () => {
 					</p>
 				</div>
 
-				<div className="col-sm-12 col-lg-4 mt-3">
+				<div className="col-sm-12 col-lg-4 mt-3 col4-desktop">
 					<button type="button" className="button_marino_great btn-review mt-5">
 						Hacer una review
 					</button>
-
-					<h3 className="black_h2 subtitle3-desktop mt-4 mb-3 ">Los presentamos de forma fácil y visual</h3>
-					<img className="img3 mt-5" src={reviewMetrics} />
+					<h3 className="black_h2 subtitle3-desktop mt-4 mb-3 mx-auto ">
+						Los presentamos de forma fácil y visual
+					</h3>
+					<img className="img3 img3-desktop mt-5" src={reviewMetrics} />
+					<p className="paragraph2-desktop">
+						Validamos y tratamos <span className="font-weight-bold">los datos,</span> para que puedas
+						comparar y evaluar cada centro de un vistazo.
+					</p>
 				</div>
 
 				<div className="col-sm-12 col-lg-4 mt-3">
@@ -74,6 +79,7 @@ const LandingPage = () => {
 						Validamos y tratamos <span className="font-weight-bold">los datos,</span> para que puedas
 						comparar y evaluar cada centro de un vistazo.
 					</p>
+					<img className="img3-responsive mt-5" src={reviewMetrics} />
 				</div>
 			</div>
 		</div>
