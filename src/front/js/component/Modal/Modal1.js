07f9116c3ec6d0031fd6209d434206a553c1adbd
Kty <katiuska.rodriguez5@gmail.com>
2021-07-17 09:10:00 +0000

    wip

--- src/front/js/component/Modal/Modal1.js
+++ src/front/js/component/Modal/Modal1.js
@@ -1,51 +1,51 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { useState } from "react";
+// import { Button } from "react-bootstrap";
+// import { useState } from "react";
 
 import "./Modal.scss";
 
-const Modal = () => {
-	const [show, setShow] = useState(false);
+// const Modal = () => {
+// 	const [show, setShow] = useState(false);
 
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+// 	const handleClose = () => setShow(false);
+// 	const handleShow = () => setShow(true);
 
-	return (
-		<>
-			<Button variant="primary" onClick={handleShow}>
-				Launch demo modal
-			</Button>
-
-			<Modal show={show} onHide={handleClose}>
-				<Modal.Header closeButton>
-					<Modal.Title>Modal heading</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>Woohoo, re reading this text in a modal!</Modal.Body>
-				<Modal.Footer>
-					<Button variant="secondary" onClick={handleClose}>
-						Close
-					</Button>
-					<Button variant="primary" onClick={handleClose}>
-						Save Changes
-					</Button>
-				</Modal.Footer>
-			</Modal>
-		</>
-	);
-};
-
-// const Modal = props => {
 // 	return (
 // 		<>
-// 			<div className="modal-dialog" role="document">
-// 				<div className="modal-content">
-// 					<div className="cross">{props.cross}</div>
-// 					<div className="arrow">{props.arrow}</div>
-// 					{props.body}
-// 				</div>
-// 			</div>
+// 			<Button variant="primary" onClick={handleShow}>
+// 				Launch demo modal
+// 			</Button>
+
+// 			<Modal show={show} onHide={handleClose}>
+// 				<Modal.Header closeButton>
+// 					<Modal.Title>Modal heading</Modal.Title>
+// 				</Modal.Header>
+// 				<Modal.Body>Woohoo, re reading this text in a modal!</Modal.Body>
+// 				<Modal.Footer>
+// 					<Button variant="secondary" onClick={handleClose}>
+// 						Close
+// 					</Button>
+// 					<Button variant="primary" onClick={handleClose}>
+// 						Save Changes
+// 					</Button>
+// 				</Modal.Footer>
+// 			</Modal>
 // 		</>
 // 	);
 // };
+
+const Modal = props => {
+	return (
+		<>
+			<div className="modal-dialog" role="document">
+				<div className="modal-content">
+					<div className="cross">{props.cross}</div>
+					<div className="arrow">{props.arrow}</div>
+					{props.body}
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default Modal;
