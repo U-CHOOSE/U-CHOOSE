04348f9a97d73edec7076099254959b19386a3cc
tapuks <david_berdiell@hotmail.com>
2021-07-17 09:55:07 +0000

    delete tmg thanks

--- src/front/js/component/Thanks/Thanks.js
+++ src/front/js/component/Thanks/Thanks.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "../Forms/Forms.scss";
 
-import { useHistory } from "react-router";
-
 const Thanks = props => {
 	return (
 		<>
@@ -12,7 +10,6 @@ const Thanks = props => {
 			<div>
 				{" "}
 				<h1 className="violet_h1"> ¡Gracias!</h1>
-				<img onClick={() => history.push("/")} src={logo} alt="" />
 				<h2>{props.subtitle}</h2>
 			</div>
 			<div>{props.buttons}</div>
