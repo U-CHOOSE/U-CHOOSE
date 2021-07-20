28d3d64c4861f6f08a89708aa700b97b21b2421b
tapuks <david_berdiell@hotmail.com>
2021-07-20 18:17:00 +0000

    reviewteacher ok

--- src/front/js/pages/ReviewTeacher3.js
+++ src/front/js/pages/ReviewTeacher3.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import "../../styles/reviewteacher.scss";
+import img1 from "../../../../docs/assets/img/Status=Empty, Ranking=1.jpg";
 //Dinamism
 const ReviewTeacher3 = () => {
 	const history = useHistory();
@@ -15,6 +16,9 @@ const ReviewTeacher3 = () => {
 			</div>
 
 			<h1 className="">Dinamismo en sus clases</h1>
+			<a>
+				<img className="icons-review" src={img1} />
+			</a>
 			<button
 				className="button_violet_small button__search"
 				onClick={() => history.push("/reviewteacher/id/university/pasion")}>
