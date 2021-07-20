12de7dffc0686f408636ba197ebe4097162c2e80
tapuks <david_berdiell@hotmail.com>
2021-07-20 20:20:16 +0000

    icons add

--- src/front/js/pages/ReviewTeacher3.js
+++ src/front/js/pages/ReviewTeacher3.js
@@ -2,9 +2,27 @@ import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import "../../styles/reviewteacher.scss";
 import img1 from "../../../../docs/assets/img/Status=Empty, Ranking=1.jpg";
+import img1full from "../../../../docs/assets/img/Status=Full, Ranking=1.jpg";
+import img2 from "../../../../docs/assets/img/Status=Empty, Ranking=2.jpg";
+
 //Dinamism
 const ReviewTeacher3 = () => {
 	const history = useHistory();
+
+	const listUrl = [img1, img2, img1full];
+	// const iterator = listIcons.keys();
+	const listImg = [];
+	const state = 4;
+	const newArray = listUrl.map((url, index) => {
+		if (index < state) {
+			listImg.push(
+				<a key={index}>
+					<img className="icons-review" src={url} />
+				</a>
+			);
+		}
+	});
+
 	return (
 		<div className="reviewTeacher3 mx-auto">
 			<div className="d-flex">
@@ -16,12 +34,16 @@ const ReviewTeacher3 = () => {
 			</div>
 
 			<h1 className="">Dinamismo en sus clases</h1>
-			<a>
-				<img className="icons-review" src={img1} />
-			</a>
-			<a>
-				<img className="icons-review" src={img1} />
-			</a>
+			<div className="container-icons">
+				{/* <a onClick={clickIcon()}>
+					<img className="icons-review" src={img1} />
+				</a>
+				<a>
+					<img className="icons-review" src={img2} />
+				</a> */}
+				{listImg}
+			</div>
+
 			<button
 				className="button_violet_small button__search"
 				onClick={() => history.push("/reviewteacher/id/university/pasion")}>
