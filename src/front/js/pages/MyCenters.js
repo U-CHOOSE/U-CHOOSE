36fd75d668c993e1507fae557f5a1ad8984936c7
Franc Cosp <franccospmeyaeso@gmail.com>
2021-07-27 09:45:17 +0000

    some changes

--- src/front/js/pages/MyCenters.js
+++ src/front/js/pages/MyCenters.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
 import "../../styles/myCenters.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 import Search from "../component/Search/Search";
 
 const MyCenters = () => {
