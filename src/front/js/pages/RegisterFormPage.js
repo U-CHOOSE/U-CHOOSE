8f58e2d4d3d566f83a7214a06aa995924e5e9562
Franc Cosp <franccospmeyaeso@gmail.com>
2021-07-29 18:05:06 +0000

    saving the forms

--- src/front/js/pages/RegisterFormPage.js
+++ src/front/js/pages/RegisterFormPage.js
@@ -13,16 +13,61 @@ const RegisterFormPage = () => {
 	const { store, actions } = useContext(Context);
 	const history = useHistory();
 	const [show, setShow] = useState(true);
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		fetch(process.env.BACKEND_URL + "/schools")
+			.then(res => res.json())
+			.then(data => setData(data))
+			.catch(err => console.log(err));
+	}, []);
 	const [checked, setChecked] = useState({
 		student: false,
 		teacher: false
 	});
 
+	//
+
+	console.log(data);
+	const handlePut = () => {
+		console.log();
+		const options = {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				user_id: JSON.parse(localStorage.getItem("id_user")),
+				school_id: JSON.parse(localStorage.getItem("selected_item")).id,
+				sign_completed: true
+			})
+		};
+
+		fetch(process.env.BACKEND_URL + "/user", options)
+			.then(res => {
+				console.log(res);
+
+				if (res.status === 201) {
+					alert("ok");
+					actions.setUpStep();
+				} else {
+					alert("failed to fetch");
+				}
+				console.log(status);
+				return res.json();
+			})
+			.then(json => localStorage.setItem("id_user", json.body.user_id))
+			.catch(error => console.log(error));
+	};
+
+	const isLoged = () => {
+		actions.login(email, password);
+	};
+
 	if (store.step === 0) {
 		return (
 			<>
-				{/* <button onClick={() => setShow(!show)} /> */}
-				{show ? (
+				{/* <button onClick={() => setShow(!show)} />
+ 				{show ? (
 					<Modal
 						cross={
 							<div className="text-right w-100" onClick={() => setShow(!show)}>
@@ -35,9 +80,7 @@ const RegisterFormPage = () => {
 						body={
 							<>
 								<h1 className="violet_h1_forms">Registro</h1>
-
-								<h4 className="colaborarUchoose">¿Cómo quieres colaborar con u-choose?</h4>
-
+								<h5>¿Cómo quieres colaborar con u-choose?</h5>
 								<label className="container" htmlFor="student">
 									<input
 										type="radio"
@@ -68,16 +111,47 @@ const RegisterFormPage = () => {
 									/>
 									Soy profesor
 								</label>
-
 								<button onClick={() => actions.setUpStep()} className="button_violet_small register">
 									Siguiente
 								</button>
-							</>
+							</> */}
+				{/* <button onClick={() => setShow(!show)}>Modal</button> */}
+				<h1 className="violet_h1_forms">Registro</h1>
+				<h2>¿Cómo quieres colaborar con u-choose?</h2>
+				<label className="container" htmlFor="student">
+					<input
+						type="radio"
+						value={true}
+						id="student"
+						checked={checked.student}
+						onClick={() =>
+							setChecked({
+								teacher: false,
+								student: !checked.student
+							})
 						}
 					/>
-				) : (
-					""
-				)}
+					Soy alumno
+				</label>
+				<label className="container" htmlFor="teacher">
+					<input
+						type="radio"
+						value={true}
+						id="teacher"
+						checked={checked.teacher}
+						onClick={() =>
+							setChecked({
+								teacher: !checked.teacher,
+								student: false
+							})
+						}
+					/>
+					Soy professor
+				</label>
+				<button onClick={() => actions.setUpStep()} className="button_violet_small register">
+					Siguiente
+				</button>
+				: ""
 			</>
 		);
 	} else if (store.step === 1) {
@@ -90,7 +164,7 @@ const RegisterFormPage = () => {
 						<BsArrowLeft />{" "}
 					</div>
 				}
-				body={checked.student ? <StudentForm /> : <TeacherForm />}
+				body={checked.student === true ? <StudentForm /> : <TeacherForm />}
 			/>
 		);
 	} else if (store.step === 2) {
@@ -109,10 +183,14 @@ const RegisterFormPage = () => {
 							title="¿Dónde has estudiado?"
 							placeholder="Busca un centro"
 							span1="¿No encuentras tu centro?"
-							// data = {
-							// 	//didnt'have he endpoint to recived the data
-							// }
+							data={data}
+							type="schools"
 							span2="Saltar este paso"
+							button={
+								<button onClick={handlePut} className="button_violet_small">
+									Siguiente
+								</button>
+							}
 						/>
 					) : (
 						<Search
@@ -120,9 +198,13 @@ const RegisterFormPage = () => {
 							title="¿Dónde has dado clase?"
 							placeholder="Busca un centro"
 							span1="¿No encuentras tu centro?"
-							// data = {
-							// 	//didnt'have he endpoint to recived the data
-							// }
+							data={data}
+							type="schools"
+							button={
+								<button onClick={handlePut} className="button_violet_small">
+									Siguiente
+								</button>
+							}
 						/>
 					)
 				}
@@ -195,7 +277,6 @@ const RegisterFormPage = () => {
 					body={
 						<>
 							<Thanks
-								className="colaborarUchoose"
 								subtitle="Has completado tu registro, ¿Quieres que te ayudemos a tomar una decisión sobre tu futuro?"
 								buttons={
 									<>
