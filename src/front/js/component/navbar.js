f13c3e0ff88105ec024ecab121786ba7b3417154
Kty <katiuska.rodriguez5@gmail.com>
2021-08-02 14:36:26 +0000

    sin funcionar dropDow a la izq

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,9 +5,11 @@ import { BsBuilding } from "react-icons/bs";
 import { BsFillPersonFill } from "react-icons/bs";
 import "../../styles/navbar.scss";
 import imgIcon from "../../img/imgIcon.png";
-import Modal from "./Modal/Modal1";
+import { useContext } from "react";
+import { Context } from "../store/appContext";
+import { Navbar, NavDropdown,Navmenu, Container, Button } from "react-bootstrap";
 
-import { Navbar, Nav, NavDropdown, NavLink, NavCollapse, Container, Button } from "react-bootstrap";
+// import { Navbar, Nav, NavDropdown, NavLink, NavCollapse, Container, Button } from "react-bootstrap";
 
 const NavbarComp = () => {
 	const history = useHistory();
@@ -22,11 +24,11 @@ const NavbarComp = () => {
 						<img onClick={() => history.push("/")} src={imgIcon} alt="" />
 						{/* <span className="navbar-toggler-icon" />{" "} */}
 					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 					{/* <Navbar.Toggle aria-controls="basic-nav-dropdown" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="me-auto"> */}
-					<NavDropdown className="  navbar-toggler-icon">
+					
+					<NavDropdown className="navbar-toggler-icon">
 						{/* <span className="navbar-toggler-icon" />{" "} */}
 						<NavDropdown.Item href="" onClick={() => history.push("/registerformpage")}>
 							<h3>Registrate</h3>
@@ -52,62 +54,14 @@ const NavbarComp = () => {
 					</NavDropdown>
 					{/* </Nav>
 					</Navbar.Collapse>{" "} */}
+				
 				</Container>
 			</Navbar>
-			{show ? <Modal body={screen} /> : ""}{" "}
+			{/* {show ? <Modal body={screen} /> : ""}{" "}  */}
 		</>
 	);
 };
 
 export default NavbarComp;
 
-// 		<>
-// 			<Navbar bg="ligth" expand="lg">
-// 				<Container>
-// 					<Navbar.Brand href="#home">
-// 						<img onClick={() => history.push("/")} src={imgIcon} alt="" />
-// 						{/* <span className="navbar-toggler-icon" />{" "} */}
-// 					</Navbar.Brand>
-// 					{/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
-// 					{/* <Navbar.Collapse id="responsive-navbar-nav"> */}
-// 						{/* <Navbar.Toggle aria-controls="basic-nav-dropdown" /> */}
-// 						{/* <Navbar.Collapse id="basic-navbar-nav"> */}
-// 						<Nav className="me-auto">
-// 							{/* <Nav.Link href="#features">Centros</Nav.Link>
-// 							<Nav.Link href="#features">Profesores</Nav.Link> */}
-// 							 <NavDropdown className="  navbar-toggler-icon   ">
-// 							{/* <span className="navbar-toggler-icon" />{" "} */}
 
-// 							{/* <Navbar> */}
-// 								 <NavDropdown.Item href="" onClick={() => history.push("/registerformpage")}>
-// 									<h3>Registrate</h3>
-// 								</NavDropdown.Item>
-// 								<br />
-// 								<NavDropdown.Item href="#action/3.1" onClick={() => history.push("/searchschools")}>
-// 									{/* <BsBuilding /> */}
-// 									<h4>Centros</h4>
-// 								</NavDropdown.Item>
-// 								<br />
-// 								<NavDropdown.Item href="#action/3.2" onClick={() => history.push("searchteachers")}>
-// 									 <div className="d-flex justify-content-around ">
-// 									{/* <BsFillPersonFill /> */}
-
-// 									<h4>Profesores</h4>
-// 									 </div>
-// 								</NavDropdown.Item>
-// 								<br />
-// 								<NavDropdown.Item className="btnNav" onClick={() => history.push("/registerformpage")}>
-// 									<h3>Registrate</h3>
-// 								{/* </NavDropdown.Item>
-// 							</Navbar> */}
-// 						{/* </Nav> */}
-// 						{/* </Nav> */}
-// 					{/* </Navbar.Collapse> */}
-// 				</Container>
-// 			</Navbar>
-// 			{/* {show ? <Modal body={screen} /> : ""}{" "} */}
-// 		</>
-// 	);
-// };
-
-// export default NavbarComp;
